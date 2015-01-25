095040425b1728ca44dac7e5aa0f70efd9721534
James Chen <jjc51@yahoo.com>
2015-01-25 15:04:41 -0800

    Update plot1.R

--- Project2/plot1.R
+++ Project2/plot1.R
@@ -1,4 +1,5 @@
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008
+source(load_data.R)
 aggNEI <- aggregate(Emissions ~ year,NEI, sum)
 
 #Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008
